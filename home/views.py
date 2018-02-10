b03f87b38732ca38257e58301b78fe617ba85a8b
Ivan Neychev <ivan_tn@abv.bg>
2018-02-10 18:22:32 +0200

    papcho 46

--- home/views.py
+++ home/views.py
@@ -11,3 +11,11 @@ class HomeView(TemplateView):
         form = HomeForm
         return render(request, self.template_name, {'form': form})
 
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            form = HomeForm()
+
+            args = {'form': form, 'text': text}
+            return render(request, self.template_name, args)
