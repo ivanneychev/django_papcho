93386a920ea33dbe4996a65a8905fdcb38843752
Ivan Neychev <ivan_tn@abv.bg>
2018-02-10 18:28:39 +0200

    papcho 46_1

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from home.forms import HomeForm
 
@@ -15,7 +15,7 @@ class HomeView(TemplateView):
         form = HomeForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['post']
-            form = HomeForm()
+            return redirect('home:home')
 
-            args = {'form': form, 'text': text}
-            return render(request, self.template_name, args)
+        args = {'form': form, 'text': text}
+        return render(request, self.template_name, args)
