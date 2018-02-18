11ad3edbd77929082ce14f8dece57ce4d7a1aadb
Ivan Neychev <ivan_tn@abv.bg>
2018-02-18 22:20:05 +0200

    papcho 57

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from home.views import HomeView
+from home.views import change_friends
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home')
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$',
+        change_friends, name='change_friends')
 ]
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
 
 
 class HomeView(TemplateView):
@@ -34,3 +34,12 @@ class HomeView(TemplateView):
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
 
+
+def change_friends(request, operation, pk):
+    new_friend = User.objects.get(pk=pk)
+
+    if operation == 'add':
+        Friend.make_friend(request.user, new_friend)
+    elif operation == 'remove':
+        Friend.lose_friend(request.user, new_friend)
+    return redirect('home:home')
